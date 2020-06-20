461778c841e61a31cd841ef777eb81beb5614770
Devon <devon.meza@yahoo.com>
2020-06-19 17:01:24 -0700

    deleted unecessary files

--- src/index.js
+++ src/index.js
@@ -1,13 +1,53 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+class Clicker extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {clicks: 0};
+
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  componentDidMount() {
+
+  }
+
+  componentWillUnmount() {
+    this.setState({
+      clicks: 0,
+    });
+  }
+
+
+  handleClick() {
+    console.log("clicked");
+    this.setState((state,props) => ({
+      clicks: state.clicks + 1,
+    }));
+  }
+
+  render() {
+    return(
+      <div>
+      <h1 class="welcome_message">Hello welcome to Dev's MAGNIFICANT counter...</h1>
+      <button onClick={this.handleClick} class="button"> Click </button>
+      {this.state.clicks > 0 && 
+        <h2>Wonderful! You have clicks</h2>
+      }
+      <h2>{this.state.clicks}</h2>
+      </div>
+    ) 
+  }
+
+}
+
+
+ReactDOM.render( 
+  <Clicker />,
   document.getElementById('root')
 );
 
